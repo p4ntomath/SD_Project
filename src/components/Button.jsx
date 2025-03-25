1cf022c07ae2faa36f87171b45a7f1bb40c38705
Mahlatse <morwawarabothata5@gmail.com>
2025-03-25 21:56:59 +0200

    added

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Button(){
+    return (
+        <div>
+            <button>Click me</button>
+            <p>Hello</p>
+        </div>
+    )
+}
---
+++ src/components/Text.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Text(){
+    return (
+        <div>
+            <p>Text</p>
+        </div>
+    )
+}
