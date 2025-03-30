dc27cd07b55566504fd76ccd3723fde59032fb97
Mahlatse <morwawarabothata5@gmail.com>
2025-03-30 20:04:09 +0200

    removed example components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
-import Text from './components/Text'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Text/>
     </div>
   )
 }
