e8dc297143f1002f2135ceab1f5d00a378177178
Diana Bingani <2709394@students.wits.ac.za>
2025-03-31 14:48:33 +0200

    added logIn page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
-import WelcomePage from './pages/welcomePage.jsx'
+import WelcomePage from './pages/welcomePage.jsx';
+import LoginPage from './pages/loginPage.jsx';
+
+
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<WelcomePage />} /> 
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
